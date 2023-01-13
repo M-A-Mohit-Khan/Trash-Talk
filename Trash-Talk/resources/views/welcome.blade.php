b3a9c95684ad7660a8f37666b21ebde46a32deb3
AdnanShakib <adnanshakib1999@gmail.com>
2023-01-13 22:35:07 +0600

    adnan first commit

--- Trash-Talk/resources/views/welcome.blade.php
+++ Trash-Talk/resources/views/welcome.blade.php
@@ -126,6 +126,7 @@
                         Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})
                     </div>
                 </div>
+                adnan shakib
             </div>
         </div>
     </body>
